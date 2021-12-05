5bb71976d6e8c7997ae2cfc921a4ea1df9215a62
AhsanAkib <70332106+AhsanAkib@users.noreply.github.com>
2021-12-05 12:21:13 +0600

    Add files via upload

---
+++ User.java
@@ -0,0 +1,14 @@
+public class User
+{
+	String uname;
+	String pass;
+	String email;
+	String phone;
+	public User(){}
+	public User(String username, String password)
+	{
+		this.uname = username;
+		this.pass = password;
+	}
+	
+} 
